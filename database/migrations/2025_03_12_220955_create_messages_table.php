5ff200488a8f6578c312bf8338578864cfd5b33a
unknown <terrariaa81@gmail.com>
2025-03-21 20:22:37 +0200

    start project

---
+++ database/migrations/2025_03_12_220955_create_messages_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('topic_id')->constrained("topics", "id")->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained("users", "id")->cascadeOnDelete();
+            $table->text('text')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('messages');
+    }
+};
